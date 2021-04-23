7bc52df878194dbdb96480e6d40b2b8c863a1e20
vina97 <vinai@hotmail.it>
2021-04-23 15:18:17 +0200

    Add new possible implementation

--- main.py
+++ main.py
@@ -1,126 +1,136 @@
+import padding_functions as pf
+import DBLSTM as net
+import numpy as np
+from Dataset import DataSet
+import data_preparation as dp
 import argparse
 import sys
-import json 
-sys.path.insert(0,'./utils')
-import data_preparation as dp
-import padding_functions as pf 
-from Dataset import DataSet
-import numpy as np
-import DBLSTM as net
+import json
+sys.path.insert(0, './utils')
 
 
 config_file = "config.json"
 
-config_run = {} # set what to run and what not to
+config_run = {}  # set what to run and what not to
+
 
 def set_global_variables(file_name):
-  with open(file_name) as json_file:
-    _dict = json.load(json_file)
-  global PATH_TO_TRAIN
-  global PATH_TO_TEST
-  global PATH_FOLDING
-  global CHECKPOINT_PATH 
-  global NUM_CLASSES
-  global log_file_path
-  global config_run
-  global config_mfcc
-  global SQ
-  global BATCH_SIZE
-  global EPOCHS
-  global HIDDEN_UNITS
-  global LR
-  global DropOut
-  PATH_TO_TRAIN = _dict["TRAIN_PATH"]
-  PATH_TO_TEST = _dict["TEST_PATH"]
-  PATH_FOLDING = _dict["FOLDING_DICT"]
-  CHECKPOINT_PATH = _dict["CHECKPOINT_PATH"]
-  log_file_path = _dict["LOG_FILE"]
-  NUM_CLASSES = _dict["NUM_CLASSES"]
-  config_run["process_data"] = _dict["propPROCESS_DATA"]
-  config_run["process_phonemes"] = _dict["propUPDATE_PHONEMES"]
-  config_run["propTRAIN"] = _dict["propTRAIN"]
-  config_run["propLOAD"] = _dict["propLOAD"]
-  config_run["propRMSilencePostLoad"] = _dict['propRMSilencePostLoad']
-  config_mfcc = _dict["MFCC_DATA"]
-  SQ = _dict["SQ"]
-  BATCH_SIZE = _dict["BATCH_SIZE"]
-  EPOCHS = _dict["EPOCHS"]
-  HIDDEN_UNITS = _dict["HIDDEN_UNITS"]
-  LR = _dict["LR"]
-  DropOut = _dict["DROPOUT"] 
+    with open(file_name) as json_file:
+        _dict = json.load(json_file)
+    global PATH_TO_TRAIN
+    global PATH_TO_TEST
+    global PATH_FOLDING
+    global CHECKPOINT_PATH
+    global NUM_CLASSES
+    global log_file_path
+    global config_run
+    global config_mfcc
+    global SQ
+    global BATCH_SIZE
+    global EPOCHS
+    global HIDDEN_UNITS
+    global LR
+    global DropOut
+    PATH_TO_TRAIN = _dict["TRAIN_PATH"]
+    PATH_TO_TEST = _dict["TEST_PATH"]
+    PATH_FOLDING = _dict["FOLDING_DICT"]
+    CHECKPOINT_PATH = _dict["CHECKPOINT_PATH"]
+    log_file_path = _dict["LOG_FILE"]
+    NUM_CLASSES = _dict["NUM_CLASSES"]
+    config_run["process_data"] = _dict["propPROCESS_DATA"]
+    config_run["process_phonemes"] = _dict["propUPDATE_PHONEMES"]
+    config_run["propTRAIN"] = _dict["propTRAIN"]
+    config_run["propLOAD"] = _dict["propLOAD"]
+    config_run["propRMSilencePostLoad"] = _dict['propRMSilencePostLoad']
+    config_mfcc = _dict["MFCC_DATA"]
+    SQ = _dict["SQ"]
+    BATCH_SIZE = _dict["BATCH_SIZE"]
+    EPOCHS = _dict["EPOCHS"]
+    HIDDEN_UNITS = _dict["HIDDEN_UNITS"]
+    LR = _dict["LR"]
+    DropOut = _dict["DROPOUT"]
+
+    global PHONEME_WISE
+    PHONEME_WISE = _dict["PHONEME_WISE"]
+
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='Process some integers.')
-  parser.add_argument('config_file', type=str, nargs=1, help='File with training Configuration')
-  args = parser.parse_args()
-  config_file = args.config_file[0]
-  set_global_variables(config_file)
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('config_file', type=str, nargs=1,
+                        help='File with training Configuration')
+    args = parser.parse_args()
+    config_file = args.config_file[0]
+    set_global_variables(config_file)
 
-  if config_run["process_data"]: # process files 
-    # Load all paths 
-    train_paths = dp.paths_from_region(PATH_TO_TRAIN)
-    test_paths = dp.paths_from_region(PATH_TO_TEST)
-    print(f"Files for train: {len(train_paths)}")
-    print(f"Files for test: {len(test_paths)}")
+    if config_run["process_data"]:  # process files
+        # Load all paths
+        train_paths = dp.paths_from_region(PATH_TO_TRAIN)
+        test_paths = dp.paths_from_region(PATH_TO_TEST)
+        print(f"Files for train: {len(train_paths)}")
+        print(f"Files for test: {len(test_paths)}")
 
-    # Compute MFCCs
-    train_dict_x = dp.compute_mfcc(train_paths, config_mfcc)
-    print(f"###  All data loded: {len(train_paths) == len(train_dict_x.keys())} ###")
-    test_dict_x = dp.compute_mfcc(test_paths, config_mfcc)
-    print(f"###  All data loded: {len(test_paths) == len(test_dict_x.keys())} ###")
+        # Compute MFCCs
+        train_dict_x = dp.compute_mfcc(train_paths, config_mfcc)
+        print(
+            f"###  All data loded: {len(train_paths) == len(train_dict_x.keys())} ###")
+        test_dict_x = dp.compute_mfcc(test_paths, config_mfcc)
+        print(
+            f"###  All data loded: {len(test_paths) == len(test_dict_x.keys())} ###")
 
-    
-    if config_run["process_phonemes"]: # substitute phonemes
-      # load phonemes dictionary
-      dict_f = dp.load_json_dict(PATH_FOLDING)
-      set_phns = set()
-      for p in (train_paths + test_paths):
-        phn_file = p + ".PHN"
-        dp.substitute_phonemes(phn_file, foldings=dict_f)
-        setphn = dp.get_phonemes(phn_file)
-        set_phns = set_phns.union(setphn)
-      phonemes_dict = {i:phn for i, phn in enumerate(list(set_phns))}
-      phonemes_dict_inv = {v:k for k,v in phonemes_dict.items()}
-      
-      dp.save_json_dict(phonemes_dict,'label_dict.json')
-      dp.save_json_dict(phonemes_dict_inv,'label_inv_dict.json')
-  
-    else: # load phonemes
-      phonemes_dict = dp.load_json_dict('label_dict.json')
-      phonemes_dict_inv = dp.load_json_dict('label_inv_dict.json')
+        if config_run["process_phonemes"]:  # substitute phonemes
+            # load phonemes dictionary
+            dict_f = dp.load_json_dict(PATH_FOLDING)
+            set_phns = set()
+            for p in (train_paths + test_paths):
+                phn_file = p + ".PHN"
+                dp.substitute_phonemes(phn_file, foldings=dict_f)
+                setphn = dp.get_phonemes(phn_file)
+                set_phns = set_phns.union(setphn)
+            phonemes_dict = {i: phn for i, phn in enumerate(list(set_phns))}
+            phonemes_dict_inv = {v: k for k, v in phonemes_dict.items()}
 
-    train_data = dp.pair_data(train_dict_x, phonemes_dict_inv)
-    dp.save_dict(train_data, 'train_data.pickle')
-    
-    test_data = dp.pair_data(test_dict_x, phonemes_dict_inv)
-    dp.save_dict(test_data, 'test_data.pickle')
+            dp.save_json_dict(phonemes_dict, 'label_dict.json')
+            dp.save_json_dict(phonemes_dict_inv, 'label_inv_dict.json')
 
-  else: # load pickles
-    phonemes_dict = dp.load_json_dict('label_dict.json')
-    phonemes_dict_inv = dp.load_json_dict('label_inv_dict.json')
-    train_data = dp.load_dict("train_data.pickle")
-    test_data = dp.load_dict("test_data.pickle")
+        else:  # load phonemes
+            phonemes_dict = dp.load_json_dict('label_dict.json')
+            phonemes_dict_inv = dp.load_json_dict('label_inv_dict.json')
 
-  if config_run["propRMSilencePostLoad"]:
-    sil_num = phonemes_dict_inv['sil']
-    train_data =  pf.remove_silence(train_data, sil_num)
-    test_data = pf.remove_silence(test_data, sil_num)
-  Xp_tr, label_tr = pf.pad_data(train_data, seq_length=SQ)
-  Xp_ts, label_ts = pf.pad_data(test_data, seq_length=SQ)
+        train_data = dp.pair_data(
+            train_dict_x, phonemes_dict_inv, PHONEME_WISE)
+        dp.save_dict(train_data, 'train_data.pickle')
 
-  train_dataset = DataSet(np.array(Xp_tr, dtype=object), np.array(label_tr, dtype=object), BATCH_SIZE)    
-  test_dataset = DataSet(np.array(Xp_ts, dtype=object), np.array(label_ts, dtype=object), BATCH_SIZE)
+        test_data = dp.pair_data(test_dict_x, phonemes_dict_inv, PHONEME_WISE)
+        dp.save_dict(test_data, 'test_data.pickle')
 
-  print("Model Crearion")
-  # model = net.DBLSTM(batch_size=BATCH_SIZE, sequence_length=SQ, n_mffc=config_mfcc["order_mfcc"],
-  #             hidden_units=HIDDEN_UNITS, out_classes=NUM_CLASSES, dropout=DropOut, num_epochs=EPOCHS, log=log_file_path,
-  #             LR=LR, ch_path=CHECKPOINT_PATH)
-  model = net._LSTM(batch_size=BATCH_SIZE, sequence_length=SQ, n_mffc=config_mfcc["order_mfcc"],
-              hidden_units=HIDDEN_UNITS, out_classes=NUM_CLASSES, dropout=DropOut, num_epochs=EPOCHS, log=log_file_path,
-               LR=LR, ch_path=CHECKPOINT_PATH)
-  
-  if config_run["propLOAD"]:
-    model.load_weights(CHECKPOINT_PATH.format(epoch=0))
-  print("Start Training")
-  if config_run["propTRAIN"]:
-    model.train_model(train_dataset, test_dataset)
+    else:  # load pickles
+        phonemes_dict = dp.load_json_dict('label_dict.json')
+        phonemes_dict_inv = dp.load_json_dict('label_inv_dict.json')
+        train_data = dp.load_dict("train_data.pickle")
+        test_data = dp.load_dict("test_data.pickle")
+
+    if config_run["propRMSilencePostLoad"]:
+        sil_num = phonemes_dict_inv['sil']
+        train_data = pf.remove_silence(train_data, sil_num)
+        test_data = pf.remove_silence(test_data, sil_num)
+    Xp_tr, label_tr = pf.pad_data(train_data, seq_length=SQ)
+    Xp_ts, label_ts = pf.pad_data(test_data, seq_length=SQ)
+
+    train_dataset = DataSet(np.array(Xp_tr, dtype=object), np.array(
+        label_tr, dtype=object), BATCH_SIZE)
+    test_dataset = DataSet(np.array(Xp_ts, dtype=object),
+                           np.array(label_ts, dtype=object), BATCH_SIZE)
+
+    print("Model Crearion")
+    # model = net.DBLSTM(batch_size=BATCH_SIZE, sequence_length=SQ, n_mffc=config_mfcc["order_mfcc"],
+    #             hidden_units=HIDDEN_UNITS, out_classes=NUM_CLASSES, dropout=DropOut, num_epochs=EPOCHS, log=log_file_path,
+    #             LR=LR, ch_path=CHECKPOINT_PATH)
+    model = net._LSTM(batch_size=BATCH_SIZE, sequence_length=SQ, n_mffc=config_mfcc["order_mfcc"],
+                      hidden_units=HIDDEN_UNITS, out_classes=NUM_CLASSES, dropout=DropOut, num_epochs=EPOCHS, log=log_file_path,
+                      LR=LR, ch_path=CHECKPOINT_PATH)
+
+    if config_run["propLOAD"]:
+        model.load_weights(CHECKPOINT_PATH.format(epoch=0))
+    print("Start Training")
+    if config_run["propTRAIN"]:
+        model.train_model(train_dataset, test_dataset)
--- utils/data_preparation.py
+++ utils/data_preparation.py
@@ -5,176 +5,202 @@ import numpy as np
 import pandas as pd
 import pickle
 
+
 def paths_from_region(path_to_data):
-  regions = os.listdir(path_to_data)
-  _paths = []
-  for region in regions:
-    speakers = os.listdir(path_to_data + region + "/")
-    for speaker in speakers:
-      speaker_path = path_to_data + region + "/" + speaker
-      speaches_path_full = os.listdir(speaker_path)
-      names = [speaker_path + "/" + f for f in set([file.split(".")[0] for file in speaches_path_full])]
-      _paths += names
-  return _paths
+    regions = os.listdir(path_to_data)
+    _paths = []
+    for region in regions:
+        speakers = os.listdir(path_to_data + region + "/")
+        for speaker in speakers:
+            speaker_path = path_to_data + region + "/" + speaker
+            speaches_path_full = os.listdir(speaker_path)
+            names = [speaker_path + "/" +
+                     f for f in set([file.split(".")[0] for file in speaches_path_full])]
+            _paths += names
+    return _paths
+
 
 def load_json_dict(file_name):
-  with open(file_name) as json_file:
-    foldings_dict = json.load(json_file)
-  return foldings_dict
+    with open(file_name) as json_file:
+        foldings_dict = json.load(json_file)
+    return foldings_dict
+
 
 def save_json_dict(data, file_name):
-  with open(file_name, 'w') as outfile:
-    json.dump(data, outfile)
+    with open(file_name, 'w') as outfile:
+        json.dump(data, outfile)
 
 
-def substitute_phonemes(file, 
-        sentences=False, # should we apply !ENTER/!EXIT for start/end?
-        foldings={}, # substitute phones with foldings dict
-        startend_sil=False): # should we substitute start and end w/ sil
-  fullname = file
-  # print(fullname)
-  phones_before = []
-  phones_after = []
-  os.rename(fullname, fullname+'~')
-  fr = open(fullname+'~', 'r')
-  fw = open(fullname, 'w')
-  text_buffer = []
-  all_lines = fr.readlines()
-  # print(all_lines)
-  for line in all_lines:
-    phones_before.append(line.split()[-1]) # phone last elt of line
-    tmpline = line
-    tmpline = tmpline.replace('-', '')
-    tmp = tmpline.split()
-    for k, v in foldings.items():
-      if tmp[-1] == k:
-        tmp[-1] = v
-        tmpline = ' '.join(tmp)
-    text_buffer.append(tmpline.split())
-  first_phone = text_buffer[0][-1].strip()
-  last_phone = text_buffer[-1][-1].strip()
-  if sentences:
-    if first_phone == 'h#' or first_phone == 'sil' or first_phone == '<s>' or first_phone == '{B_TRANS}':
-      # 'h#' or 'sil' for TIMIT
-      # '<s>' for CSJ (and other XML/Thomas-like)
-      # '{B_TRANS}' for Buckeye
-      text_buffer[0] = text_buffer[0][:-1] + ['!ENTER']
-    if last_phone == 'h#' or last_phone == 'sil' or last_phone == '</s>' or last_phone == '{E_TRANS}':
-      text_buffer[-1] = text_buffer[-1][:-1] + ['!EXIT']
-  if startend_sil:
-    text_buffer[0] = text_buffer[0][:-1] + ['sil']
-    text_buffer[-1] = text_buffer[-1][:-1] + ['sil']
-  for buffer_line in text_buffer:
-    phones_after.append(buffer_line[-1])
-    fw.write(' '.join(buffer_line) + '\n')
-  fw.close()
-  fr.close()
-  os.remove(fullname+'~')
+def substitute_phonemes(file,
+                        sentences=False,  # should we apply !ENTER/!EXIT for start/end?
+                        foldings={},  # substitute phones with foldings dict
+                        startend_sil=False):  # should we substitute start and end w/ sil
+    fullname = file
+    # print(fullname)
+    phones_before = []
+    phones_after = []
+    os.rename(fullname, fullname+'~')
+    fr = open(fullname+'~', 'r')
+    fw = open(fullname, 'w')
+    text_buffer = []
+    all_lines = fr.readlines()
+    # print(all_lines)
+    for line in all_lines:
+        phones_before.append(line.split()[-1])  # phone last elt of line
+        tmpline = line
+        tmpline = tmpline.replace('-', '')
+        tmp = tmpline.split()
+        for k, v in foldings.items():
+            if tmp[-1] == k:
+                tmp[-1] = v
+                tmpline = ' '.join(tmp)
+        text_buffer.append(tmpline.split())
+    first_phone = text_buffer[0][-1].strip()
+    last_phone = text_buffer[-1][-1].strip()
+    if sentences:
+        if first_phone == 'h#' or first_phone == 'sil' or first_phone == '<s>' or first_phone == '{B_TRANS}':
+            # 'h#' or 'sil' for TIMIT
+            # '<s>' for CSJ (and other XML/Thomas-like)
+            # '{B_TRANS}' for Buckeye
+            text_buffer[0] = text_buffer[0][:-1] + ['!ENTER']
+        if last_phone == 'h#' or last_phone == 'sil' or last_phone == '</s>' or last_phone == '{E_TRANS}':
+            text_buffer[-1] = text_buffer[-1][:-1] + ['!EXIT']
+    if startend_sil:
+        text_buffer[0] = text_buffer[0][:-1] + ['sil']
+        text_buffer[-1] = text_buffer[-1][:-1] + ['sil']
+    for buffer_line in text_buffer:
+        phones_after.append(buffer_line[-1])
+        fw.write(' '.join(buffer_line) + '\n')
+    fw.close()
+    fr.close()
+    os.remove(fullname+'~')
+
 
 def get_phonemes(file_path):
-  with open(file_path, "r") as f:
-    all_lines = f.readlines()
-    phns = set([l.split(' ')[-1].strip() for l in all_lines])
-    return phns
+    with open(file_path, "r") as f:
+        all_lines = f.readlines()
+        phns = set([l.split(' ')[-1].strip() for l in all_lines])
+        return phns
+
 
 def normalize_mfcc(mfcc):
-  """Normalize mfcc data using the following formula:
-  normalized = (mfcc - mean)/standard deviation
-  Args:
-    mfcc (numpy.ndarray):
-      An ndarray containing mfcc data.
-      Its shape is [sentence_length, coefficients]
-  Returns:
-    numpy.ndarray:
-      An ndarray containing normalized mfcc data with the same shape as
-      the input.
-  """
-  means = np.mean(mfcc, 0)
-  stds = np.std(mfcc, 0)
-  return (mfcc - means)/stds
+    """Normalize mfcc data using the following formula:
+    normalized = (mfcc - mean)/standard deviation
+    Args:
+      mfcc (numpy.ndarray):
+        An ndarray containing mfcc data.
+        Its shape is [sentence_length, coefficients]
+    Returns:
+      numpy.ndarray:
+        An ndarray containing normalized mfcc data with the same shape as
+        the input.
+    """
+    means = np.mean(mfcc, 0)
+    stds = np.std(mfcc, 0)
+    return (mfcc - means)/stds
+
 
 def compute_mfcc(paths, config_mfcc):
-  _data_x = {}
-  for p in paths:
-    x, _ = librosa.load(p + ".WAV", sr=config_mfcc["sampling_frequency"])
-    mfccs = librosa.feature.mfcc(y=x, 
-                                  sr=config_mfcc["sampling_frequency"], 
-                                  n_mfcc=config_mfcc["order_mfcc"], 
-                                  n_fft=config_mfcc["n_fft"], 
-                                  hop_length=config_mfcc["hop_length"])
-    mfccs = normalize_mfcc(mfccs)
-    id_ = p.split("/")[-2] + "_" + p.split("/")[-1]
+    _data_x = {}
+    for p in paths:
+        x, _ = librosa.load(p + ".WAV", sr=config_mfcc["sampling_frequency"])
+        mfccs = librosa.feature.mfcc(y=x,
+                                     sr=config_mfcc["sampling_frequency"],
+                                     n_mfcc=config_mfcc["order_mfcc"],
+                                     n_fft=config_mfcc["n_fft"],
+                                     hop_length=config_mfcc["hop_length"])
+        mfccs = normalize_mfcc(mfccs)
+        id_ = p.split("/")[-2] + "_" + p.split("/")[-1]
 
-    _data_x[id_] = (mfccs, p)
-  return _data_x
-
-
-def read_phn(f, temp_mfcc, phonem_dict):
-  # Read PHN files
-  temp_phones = pd.read_csv(f, delimiter=" ", header=None,  names=['start', 'end', 'phone']) 
-  
-  # Get the length of the phone data
-  _, phn_len, _ = temp_phones.iloc[-1]    
-  phn_len_mill = int(phn_len/160) #160 since each frame is 10ms
-
-  if phn_len_mill < temp_mfcc.shape[1]:
-    # An array of class labels for every 10 ms in the phone data
-    # phones[2] is the phoneme annotated from 20-30 ms
-    phones = np.zeros((len(phonem_dict.keys()), phn_len_mill), dtype=int)
-    # Make sure the length of mfcc_data and phn_len_mill are equal
-    mfcc_data = temp_mfcc[:,0:phn_len_mill]
-  else:
-    phones = np.zeros((len(phonem_dict.keys()), temp_mfcc.shape[1]))
-    mfcc_data = temp_mfcc
-  
-  d = phn_len_mill - temp_mfcc.shape[1]
-
-  # Convert the string phonemes to class labels
-  for i, (s, e, phone) in enumerate(temp_phones.iloc):
-    start = int(s/160.0)
-    end = int(e/160.0)
-
-    # print(f"{start} s, {end} e, {len}")
-    phones[phonem_dict[phone], start:min(end,phones.shape[1])] = 1
-    # print(f"{phone} found at index {ALL_PHONEMES.index(phone)}, y becomes {phones[:,start:min(end,phones.shape[1])]}")
-  
-  return phones.astype(int), mfcc_data, d
+        _data_x[id_] = (mfccs, p)
+    return _data_x
 
 
+def read_phn(f, temp_mfcc, phonem_dict, phoneme_wise=False):
+    # Read PHN files
+    temp_phones = pd.read_csv(f, delimiter=" ", header=None,  names=[
+                              'start', 'end', 'phone'])
 
-#receives an entry of the dictionary with key user_sentenceID -> (mfcc, path)
-def match_data(sentence_entry, phonem_dict, verbose=False):  
-  """Match label to mfcc
-    Args:
-      sentence_entry (Tuple):
-        A tuple of two elements, (mfccs, path): mfcc:np array shape paths
-    Returns:
-      Tuple:
-        Mfccs and label paierd
-  """
-  phoneme_file = sentence_entry[1]+".PHN"
+    if phoneme_wise:
+        phones_list = []
+        mfcc_block_list = []
 
-  mfcc_data = sentence_entry[0]         
-  
-  phones, mfcc_data, d = read_phn(phoneme_file, mfcc_data, phonem_dict)
-  if verbose:
-    if d != 0:
-      if abs(d) > 500:
-        print(f"length mismatch of {d} frames {sentence_entry[-1]}")
-  return mfcc_data, phones
+    # Get the length of the phone data
+    _, phn_len, _ = temp_phones.iloc[-1]
+    phn_len_mill = int(phn_len/160)  # 160 since each frame is 10ms
+    if not phoneme_wise:
+        if phn_len_mill < temp_mfcc.shape[1]:
+            # An array of class labels for every 10 ms in the phone data
+            # phones[2] is the phoneme annotated from 20-30 ms
+            phones = np.zeros(
+                (len(phonem_dict.keys()), phn_len_mill), dtype=int)
+            # Make sure the length of mfcc_data and phn_len_mill are equal
+            mfcc_data = temp_mfcc[:, 0:phn_len_mill]
+        else:
+            phones = np.zeros((len(phonem_dict.keys()), temp_mfcc.shape[1]))
+            mfcc_data = temp_mfcc
 
-def pair_data(x_dictionay, phonem_dict):
-  result_dict = {}
-  for k,v in x_dictionay.items():
-    mfcc, y = match_data(v, phonem_dict, verbose=True)
-    result_dict[k] = {"mfcc":mfcc.T, "y":y.T, "path": v[-1]}
-  return result_dict
+    d = phn_len_mill - temp_mfcc.shape[1]
+
+    # Convert the string phonemes to class labels
+    for i, (s, e, phone) in enumerate(temp_phones.iloc):
+        start = int(s/160.0)
+        end = int(e/160.0)
+        if phoneme_wise:
+            one_hot = np.zeros(len(ALL_PHONEMES))
+            one_hot[ALL_PHONEMES.index(phone)] = 1
+            phones_list.append(one_hot)
+            mfcc_block_list.append(
+                temp_mfcc[:, start: min(end, temp_mfcc.shape[1])])
+        else:
+            # print(f"{start} s, {end} e, {len}")
+            phones[phonem_dict[phone], start:min(end, phones.shape[1])] = 1
+        # print(f"{phone} found at index {ALL_PHONEMES.index(phone)}, y becomes {phones[:,start:min(end,phones.shape[1])]}")
+
+    if phoneme_wise:
+        return np.array(phones_list), mfcc_block_list, d
+
+    return phones.astype(int), mfcc_data, d
+
+
+# receives an entry of the dictionary with key user_sentenceID -> (mfcc, path)
+def match_data(sentence_entry, phonem_dict, verbose=False, phoneme_wise=False):
+    """Match label to mfcc
+      Args:
+        sentence_entry (Tuple):
+          A tuple of two elements, (mfccs, path): mfcc:np array shape paths
+      Returns:
+        Tuple:
+          Mfccs and label paierd
+    """
+    phoneme_file = sentence_entry[1]+".PHN"
+
+    mfcc_data = sentence_entry[0]
+
+    phones, mfcc_data, d = read_phn(
+        phoneme_file, mfcc_data, phonem_dict, phoneme_wise=phoneme_wise)
+    if verbose:
+        if d != 0:
+            if abs(d) > 500:
+                print(f"length mismatch of {d} frames {sentence_entry[-1]}")
+    return mfcc_data, phones
+
+
+def pair_data(x_dictionay, phonem_dict, phoneme_wise=False):
+    result_dict = {}
+    for k, v in x_dictionay.items():
+        mfcc, y = match_data(v, phonem_dict, verbose=True,
+                             phoneme_wise=phoneme_wise)
+        result_dict[k] = {"mfcc": mfcc.T, "y": y.T, "path": v[-1]}
+    return result_dict
+
 
 def save_dict(x, path):
-  with open(path, 'wb') as f:
-    pickle.dump(x, f)
+    with open(path, 'wb') as f:
+        pickle.dump(x, f)
+
 
 def load_dict(path):
-  with open(path, 'rb') as f:
-    loaded_obj = pickle.load(f)
-    return loaded_obj
+    with open(path, 'rb') as f:
+        loaded_obj = pickle.load(f)
+        return loaded_obj
